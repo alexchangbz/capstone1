ec106c08873234a9e12c2df7e4367f5d0377b17d
Alex Chang <alexchangbz@gmail.com>
2022-03-16 20:59:37 +0800

    truffle test

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -2,6 +2,12 @@
 pragma solidity ^0.8.11;
 
 contract Token {
-    string public name = "My Name";
-
+    string public name = "DApp Token";
+    string public symbol = "DAPP";
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+    
+    constructor() {
+        totalSupply = 1000000 * 10 ** decimals;
+    }
 }
--- test/Token.test.js
+++ test/Token.test.js
@@ -1,17 +1,43 @@
-const Token = artifacts.require('./Token')
+const { assert } = require("chai")
 
-require('chai')
-    .use(require('chai-as-promised'))
-    .should()
+const Token = artifacts.require('Token')
 
 contract('Token', (accounts) => {
 
+    const name = "DApp Token"
+    const symbol = "DAPP"
+    const decimals = 18
+    const totalSupply = '1000000000000000000000000'
+
+    let token
+
+    beforeEach(async () => {
+        token = await Token.deployed()
+    })
+
     describe('deployment', () => {
         it('tracks the name', async () => {
-            const token = await Token.new()
+            
             // Read token name here
-            const result = token.name()
-            result.should.equal("My Name")
+            const result = await token.name()
+            assert.equal(result, name)
+        })
+
+        it('tracks the symbol', async () => {
+            const result = await token.symbol()
+            assert.equal(result, symbol)
+        })
+
+        it('tracks the decimals', async () => {
+            const result = await token.decimals()
+            assert.equal(result, decimals)
+        })
+
+        it('tracks the total supply', async () => {
+            const result = await token.totalSupply()
+            const conResult = result.toString()
+            console.log(conResult)
+            assert.equal(conResult, totalSupply)
         })
     })
 
